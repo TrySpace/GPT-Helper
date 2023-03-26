18720fa224608ddc7d31bfb3fc95b2b2b54dbdb8
Gasman <mgasman@gmail.com>
2023-03-26 15:56:07 +0200

    cleanup css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 
 import { createTheme, ThemeProvider } from '@mui/material/styles'
@@ -18,6 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
