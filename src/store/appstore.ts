59a153b3271a17e3b3a56792616f73196e9ea7e0
Gasman <mgasman@gmail.com>
2023-03-27 14:47:05 +0200

    log some zustand

--- src/store/appstore.ts
+++ src/store/appstore.ts
@@ -1,6 +1,8 @@
 import { PaletteMode } from '@mui/material';
 import create from 'zustand';
 
+import { devtools } from 'zustand/middleware'
+
 interface AppStore {
   theme: PaletteMode;
   loading: boolean;
@@ -10,13 +12,20 @@ interface AppStore {
   setSettingsOpen: (open: boolean) => void;
 }
 
-export const useAppStore = create<AppStore>((set) => ({
-  theme: 'dark',
-  loading: false,
-  settingsOpen: false,
-  setTheme: (theme: PaletteMode) => set({ theme }),
-  setLoading: (loading: boolean) => set({ loading }),
-  setSettingsOpen: (open: boolean) => set({ settingsOpen: open }),
-}));
+export const useAppStore = create<AppStore>()(
+  devtools(
+    ((set) => ({
+      theme: 'dark',
+      loading: false,
+      settingsOpen: false,
+      setTheme: (theme: PaletteMode) => set({ theme }, false, 'setTheme'),
+      setLoading: (loading: boolean) => set({ loading }, false, 'setLoading'),
+      setSettingsOpen: (open: boolean) => set({ settingsOpen: open }, false, 'setSettingsOpen'),
+    })),
+    {
+      name: 'gpt-app-store',
+    }
+  )
+)
 
 export default useAppStore;
--- src/store/prompt.ts
+++ src/store/prompt.ts
@@ -1,5 +1,7 @@
 import create from 'zustand/vanilla';
 
+import { devtools } from 'zustand/middleware'
+
 type PromptControllerStore = {
   temperature: number;
   tokens: number;
@@ -13,17 +15,22 @@ type PromptControllerStore = {
   setThreadSize: (size: number) => void;
 };
 
-const usePromptControllerStore = create<PromptControllerStore>((set) => ({
-  temperature: 0.5,
-  tokens: 512,
-  nucleus: 0.5,
-  selectedModel: 'gpt-3.5-turbo',
-  threadSize: 3,
-  setTemperature: (temp) => set({ temperature: temp }),
-  setTokens: (tokens) => set({ tokens }),
-  setNucleus: (nucleus) => set({ nucleus }),
-  setSelectedModel: (selectedModel) => set({ selectedModel }),
-  setThreadSize: (size) => set({ threadSize: size }),
-}));
+const usePromptControllerStore = create<PromptControllerStore>()(
+  devtools(((set) => ({
+    temperature: 0.5,
+    tokens: 512,
+    nucleus: 0.5,
+    selectedModel: 'gpt-3.5-turbo',
+    threadSize: 3,
+    setTemperature: (temp) => set({ temperature: temp }),
+    setTokens: (tokens) => set({ tokens }),
+    setNucleus: (nucleus) => set({ nucleus }),
+    setSelectedModel: (selectedModel) => set({ selectedModel }),
+    setThreadSize: (size) => set({ threadSize: size }),
+  })), {
+    name: 'prompt-controller-store',
+  }
+  )
+)
 
 export default usePromptControllerStore;
